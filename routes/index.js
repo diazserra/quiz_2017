b5e8fe5cca4aec489ca14840bacb069c91c0a2c5
Jose Angel <joseangel.miranda.diaz@alumnos.upm.es>
2017-06-05 19:12:23 +0200

    P52 Fin

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,8 @@ router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
+router.get('/quizzes/random_nomore', quizController.randomnone);
 
 
 module.exports = router;
